0cd1f4e3d8f9e668ec06d00ad11fddc02c4e01be
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-28 23:21:58 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+require'pry'
+class Artist
+    attr_accessor :name, :songs, :count
+    @@count = 0
+    def initialize(name)
+        @name = name
+        @songs = []
+    end
+
+    def add_song(song)
+        @songs << song
+        song.artist = self
+        @@count += 1
+    end
+
+    def add_song_by_name(name)
+        song = Song.new(name)
+        @songs << song
+        song.artist = self
+        @@count += 1
+    end
+
+    def self.song_count
+        @@count
+    end
+end
---
+++ lib/post.rb
@@ -0,0 +1,13 @@
+class Post
+    attr_accessor :title, :author
+    @@all = []
+
+    def initialize(title)
+        @title = title
+        @@all << self
+    end
+
+    def author_name
+        self.author ? self.author.name : nil
+    end
+end
